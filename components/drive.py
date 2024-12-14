2f861521e9dfd416ab69f928eb0499f04ec4a511
MizaGBF <backupgbf1@gmail.com>
2024-12-14 17:42:23 +0100

    Reviewed drive component

--- components/drive.py
+++ components/drive.py
@@ -16,12 +16,15 @@ import os
 # ----------------------------------------------------------------------------------------------------------------
 
 class Drive():
+    ZIP_CHUNK = 8192
+    
     def __init__(self, bot : 'DiscordBot') -> None:
         self.bot = bot
         self.debug = bot.debug_mode or bot.test_mode
         self.gauth = None
         self.gdrive = None
         try:
+            # Set GoogleAuth with service-secrets.json
             self.gauth = GoogleAuth(settings={
                         "client_config_backend": "service",
                         "service_config": {
@@ -41,12 +44,14 @@ class Drive():
             self.gdrive = None
             self.bot.logger.pushError("[DRIVE] Failed to initialize Drive component:", e, send_to_discord=False)
             raise e
+        # Raise exceptions in case of errors
 
     def init(self) -> None:
         pass
 
     """decompressJSON_gzip()
     Decompress the given byte array (which must be valid compressed gzip data) and return the decoded text (utf-8).
+    Legacy code for compatibility with very old MizaBOT saves. It shoudn't be needed anymore.
     
     Parameters
     --------
@@ -60,12 +65,13 @@ class Drive():
         with io.BytesIO() as bio:
             with io.BytesIO(inputBytes) as stream:
                 decompressor = gzip.GzipFile(fileobj=stream, mode='r')
-                while True:  # until EOF
-                    chunk = decompressor.read(8192)
-                    if not chunk:
+                while True:
+                    chunk = decompressor.read(self.ZIP_CHUNK) # decompress the next chunk
+                    if not chunk: # we reached EOF
                         decompressor.close()
-                        bio.seek(0)
-                        return bio.read().decode("utf-8")
+                        bio.seek(0) # go back to BytesIO beginning
+                        return bio.read().decode("utf-8") # read its content, decode and return
+                    # write decompressed chunk to BytesIO
                     bio.write(chunk)
                 return None
 
@@ -84,12 +90,13 @@ class Drive():
         with io.BytesIO() as bio:
             with io.BytesIO(inputBytes) as stream:
                 decompressor = lzma.LZMADecompressor()
-                while not decompressor.eof:  # until EOF
-                    chunk = decompressor.decompress(stream.read(8192), max_length=8192)
-                    if decompressor.eof:
+                while not decompressor.eof:
+                    chunk = decompressor.decompress(stream.read(self.ZIP_CHUNK), max_length=self.ZIP_CHUNK) # decompress the next chunk
+                    if decompressor.eof: # we reached EOF
                         if len(chunk) > 0: bio.write(chunk)
-                        bio.seek(0)
-                        return bio.read().decode("utf-8")
+                        bio.seek(0) # go back to BytesIO beginning
+                        return bio.read().decode("utf-8") # read its content, decode and return
+                    # write decompressed chunk to BytesIO
                     bio.write(chunk)
                 return None
 
@@ -107,16 +114,16 @@ class Drive():
     """
     def compressJSON(self, inputString : str) -> bytes:
         with io.BytesIO() as bio:
-            bio.write(inputString.encode("utf-8"))
-            bio.seek(0)
+            bio.write(inputString.encode("utf-8")) # set binary content
+            bio.seek(0) # go to beginning
             buffers = []
             compressor = lzma.LZMACompressor()
-            while True:  # until EOF
-                chunk = bio.read(8192)
-                if not chunk: # EOF?
-                    buffers.append(compressor.flush())
-                    return b"".join(buffers)
-                buffers.append(compressor.compress(chunk))
+            while True:
+                chunk = bio.read(self.ZIP_CHUNK) # read a chunk
+                if not chunk: # EOF
+                    buffers.append(compressor.flush()) # flush to finish
+                    return b"".join(buffers) # return joined compressed result
+                buffers.append(compressor.compress(chunk)) # add compressed chunk
 
     """load()
     Download save.json
@@ -128,26 +135,28 @@ class Drive():
         try:
             file_list = self.gdrive.ListFile({'q': "'" + self.bot.data.config['tokens']['drive'] + "' in parents and trashed=false"}).GetList() # get the file list in our folder
             # search the save file
-            for s in file_list:
-                if s['title'] == "save.gzip":
-                    s.GetContentFile(s['title']) # iterate until we find save.gzip and download it
-                    with open("save.gzip", "rb") as stream:
-                        with open("save.json", "w") as out:
-                            out.write(self.decompressJSON_gzip(stream.read()))
-                    os.remove("save.gzip")
-                    return True
-                elif s['title'] == "save.lzma":
-                    s.GetContentFile(s['title']) # iterate until we find save.lzma and download it
-                    with open("save.lzma", "rb") as stream:
-                        with open("save.json", "w") as out:
-                            out.write(self.decompressJSON(stream.read()))
-                    os.remove("save.lzma")
-                    return True
-            # legacy
+            for s in file_list: # iterate until we find a compressed save
+                match s['title']:
+                    case "save.gzip": # legacy compatibility for .gzip
+                        s.GetContentFile(s['title']) # download
+                        with open("save.gzip", "rb") as stream: # read file
+                            with open("save.json", "w") as out: # and uncompress
+                                out.write(self.decompressJSON_gzip(stream.read()))
+                        os.remove("save.gzip") # delete compressed version
+                        return True
+                    case "save.lzma":
+                        s.GetContentFile(s['title']) # download
+                        with open("save.lzma", "rb") as stream: # read file
+                            with open("save.json", "w") as out: # and uncompress
+                                out.write(self.decompressJSON(stream.read()))
+                        os.remove("save.lzma") # delete compressed version
+                        return True
+            # fallback, legacy compatibility for uncompressed.json
             for s in file_list:
                 if s['title'] == "save.json":
-                    s.GetContentFile(s['title']) # iterate until we find save.json and download it
+                    s.GetContentFile(s['title']) # download
                     return True
+            # not found
             return False
         except Exception as e:
             self.bot.logger.pushError("[DRIVE] Failed to load 'save.json':", e, send_to_discord=False)
@@ -167,22 +176,22 @@ class Drive():
     def save(self, data : dict) -> Optional[bool]: # write data as save.json to the folder id in tokens
         if self.debug: return None
         try:
-            # backup
+            # get file list
             file_list = self.gdrive.ListFile({'q': "'" + self.bot.data.config['tokens']['drive'] + "' in parents and trashed=false"}).GetList()
             if len(file_list) > 9: # delete if we have too many backups
                 for f in file_list:
-                    if f['title'].find('backup') == 0:
+                    if f['title'].startswith('backup'):
                         f.Delete()
             # list the previous save(s)
             prev = [f for f in file_list if f['title'] in ["save.json", "save.gzip", "save.lzma"]]
-            # compress
+            # compress the one passed as a parameter
             cdata = self.compressJSON(data)
-            # saving
+            # create file
             s = self.gdrive.CreateFile({'title':'save.lzma', 'mimeType':'application/x-lzma', "parents": [{"kind": "drive#file", "id": self.bot.data.config['tokens']['drive']}]})
             with io.BytesIO(cdata) as stream:
                 s.content = stream
-                s.Upload()
-            # rename the previous save(s)
+                s.Upload() # upload
+            # rename the previous save(s) to backup
             for f in prev:
                 f['title'] = "backup_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + "." + f['title'].split(".")[-1]
                 f.Upload()
@@ -209,9 +218,9 @@ class Drive():
         if self.debug: return None
         try:
             s = self.gdrive.CreateFile({'title':name, 'mimeType':mime, "parents": [{"kind": "drive#file", "id": folder}]})
-            with open(target, "rb") as stream:
+            with open(target, "rb") as stream: # open file
                 s.content = stream
-                s.Upload()
+                s.Upload() # and upload
             return True
         except Exception as e:
             self.bot.logger.pushError("[DRIVE] Failed to upload file '{}':", e, send_to_discord=False)
@@ -234,17 +243,18 @@ class Drive():
     def overwriteFile(self, target : str, mime : str, name : str, folder : str) -> Optional[bool]:
         if self.debug: return None
         try:
-            file_list = self.gdrive.ListFile({'q': "'" + folder + "' in parents and trashed=false"}).GetList() # get the file list in our folder
+            # get file list
+            file_list = self.gdrive.ListFile({'q': "'" + folder + "' in parents and trashed=false"}).GetList()
             for s in file_list:
-                if s['title'] == name:
-                    with open(target, "rb") as stream:
+                if s['title'] == name: # if our file is found
+                    with open(target, "rb") as stream: # update it with our local file content
                         s.content = stream
                         s.Upload()
                     return True
         except Exception as e:
             self.bot.logger.pushError("[DRIVE] Failed to overwrite file '{}':", e, send_to_discord=False)
             return False
-        # not found
+        # not found, we do a normal upload
         return self.saveDiskFile(target, mime, name, folder)
 
     """mvFile()
@@ -263,11 +273,12 @@ class Drive():
     def mvFile(self, name : str, folder : str, new : str) -> Optional[bool]:
         if self.debug: return None
         try:
-            file_list = self.gdrive.ListFile({'q': "'" + folder + "' in parents and trashed=false"}).GetList() # get the file list in our folder
+            # get file list
+            file_list = self.gdrive.ListFile({'q': "'" + folder + "' in parents and trashed=false"}).GetList()
             for s in file_list:
-                if s['title'] == name:
-                    s['title'] = new # iterate until we find the file and change name
-                    s.Upload()
+                if s['title'] == name: # the file is found
+                    s['title'] = new # change the name
+                    s.Upload() # and update
                     return True
             return False
         except Exception as e:
@@ -287,10 +298,11 @@ class Drive():
     """
     def dlFile(self, name : str, folder : str, destination : Optional[str] = None)-> Optional[bool]:
         try:
+            # get file list
             file_list = self.gdrive.ListFile({'q': "'" + folder + "' in parents and trashed=false"}).GetList() # get the file list in our folder
             for s in file_list:
-                if s['title'] == name:
-                    s.GetContentFile(s['title'] if destination is None else destination) # iterate until we find the file and download it
+                if s['title'] == name: # our file is found
+                    s.GetContentFile(s['title'] if destination is None else destination) # download it
                     return True
             return None
         except Exception as e:
