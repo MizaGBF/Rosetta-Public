287ac33cd96397d9e6eed408d4b6d0474278dbc8
MizaGBF <backupgbf1@gmail.com>
2024-12-15 15:03:05 +0100

    Reviewed page

--- views/page.py
+++ views/page.py
@@ -24,8 +24,8 @@ class Page(BaseView):
     """
     def __init__(self, bot : 'DiscordBot', owner_id : int, embeds : list, timeout : float = 180.0, enable_timeout_cleanup : bool = False) -> None:
         super().__init__(bot, owner_id=owner_id, timeout=timeout, enable_timeout_cleanup=enable_timeout_cleanup)
-        self.current = 0
-        self.embeds = embeds
+        self.current = 0 # current selected embed
+        self.embeds = embeds # list of embeds, one embed = one page
 
     """prev()
     The previous button coroutine callback.
@@ -38,8 +38,9 @@ class Page(BaseView):
     """
     @disnake.ui.button(label='◀️', style=disnake.ButtonStyle.blurple)
     async def prev(self, button: disnake.ui.Button, interaction: disnake.Interaction) -> None:
-        if not button.disabled and self.ownership_check(interaction):
+        if not button.disabled and self.ownership_check(interaction): # check if enabled and is view author
             if len(self.embeds) > 0:
+                # cycle to previous page/embed
                 self.current = (self.current + len(self.embeds) - 1) % len(self.embeds)
                 await interaction.send("\u200b", ephemeral=True, delete_after=0)
                 await self.message.edit(embed=self.embeds[self.current])
@@ -59,8 +60,9 @@ class Page(BaseView):
     """
     @disnake.ui.button(label='▶️', style=disnake.ButtonStyle.blurple)
     async def next(self, button: disnake.ui.Button, interaction: disnake.Interaction) -> None:
-        if not button.disabled and self.ownership_check(interaction):
+        if not button.disabled and self.ownership_check(interaction): # check if enabled and is view author
             if len(self.embeds) > 0:
+                # cycle to next page/embed
                 self.current = (self.current + 1) % len(self.embeds)
                 await interaction.send("\u200b", ephemeral=True, delete_after=0)
                 await self.message.edit(embed=self.embeds[self.current])
@@ -74,58 +76,68 @@ class Page(BaseView):
 # PageRanking View and dropdown
 # ----------------------------------------------------------------------------------------------------------------
 # View class used to add Previous and Next buttons to cycle between multiple embeds
-# Also add a select
+# Also add a select to browser player or crew profiles
 # ----------------------------------------------------------------------------------------------------------------
 
 class RankingDropdown(disnake.ui.StringSelect):
+    # The drop down
     def __init__(self, placeholder : str, search_results : list) -> None:
         # Define the options that will be presented inside the dropdown
-        options = [disnake.SelectOption(label=s[0], description=str(s[1])) for s in search_results]
+        options = [disnake.SelectOption(label=s[0], description=str(s[1])) for s in search_results] # List of selectable elements
         super().__init__(
             placeholder=placeholder,
             min_values=1,
             max_values=1,
             options=options
-        )
+        ) # init the disnake.ui.StringSelect
 
     def update_options(self, search_results : list) -> None:
-        options = [disnake.SelectOption(label=s[0], description=str(s[1])) for s in search_results]
-        self.options=options
+        options = [disnake.SelectOption(label=s[0], description=str(s[1])) for s in search_results] # change the list of selectable elements
+        self.options=options # update the disnake.ui.StringSelect
 
     async def callback(self, inter: disnake.MessageInteraction) -> None:
-        if not self.disabled and self.view.ownership_check(inter):
-            if len(self.view.embeds) >= 2:
+        # called when a choice is made
+        if not self.disabled and self.view.ownership_check(inter): # check if enabled and the view author
+            if len(self.view.embeds) >= 2: # disable previous and next buttons if they exist (i.e. if there are at least 2 pages)
                 self.view.prev_b.disabled=True
                 self.view.prev_b.style=disnake.ButtonStyle.secondary
                 self.view.next_b.disabled=True
                 self.view.next_b.style=disnake.ButtonStyle.secondary
-            self.view.close_b.disabled=True
+            self.view.close_b.disabled=True # disable the close button
             self.view.close_b.style=disnake.ButtonStyle.secondary
-            self.disabled=True
-            await inter.response.defer(ephemeral=True)
+            self.disabled=True # disable this dropdown
+            await inter.response.defer(ephemeral=True) # defer (for lag reasons)
             try:
-                if self.view.stype:# crew
+                if self.view.stype: # crew profile
+                    # set loading message
                     await inter.edit_original_message(embed=self.view.bot.embed(title=self.view.embeds[0].title, description="{} Loading Crew `{}`...".format(self.view.bot.emote.get('time'), self.values[0]), color=self.view.color), view=self.view)
-                    await asyncio.sleep(0.5)
+                    await asyncio.sleep(0.5) # add delay to temper user expectation and keep them from spamming
+                    # enable close button
                     self.view.close_b.disabled=False
                     self.view.close_b.style=disnake.ButtonStyle.danger
                     self.view.close_b.label = "Close"
                     self.disabled=False
+                    # call GuildWar cog _crew_sub to open the crew profile in this interaction
                     await self.view.bot.get_cog('GuildWar')._crew_sub(inter, self.values[0], 2, view=self.view)
-                else:
+                else: # player profile
+                    # set loading message
                     await inter.edit_original_message(embed=self.view.bot.embed(title=self.view.embeds[0].title, description="{} Loading Profile `{}`...".format(self.view.bot.emote.get('time'), self.values[0]), color=self.view.color), view=self.view)
-                    await asyncio.sleep(0.5)
+                    await asyncio.sleep(0.5) # add delay to temper user expectation and keep them from spamming
+                    # enable close button
                     self.view.close_b.disabled=False
                     self.view.close_b.style=disnake.ButtonStyle.danger
                     self.view.close_b.label = "Close"
                     self.disabled=False
+                    # call GranblueFantasy cog _profile to open the player profile in this interaction
                     await self.view.bot.get_cog('GranblueFantasy')._profile(inter, self.values[0], clean=True, color=self.view.color, view=self.view)
             except Exception as e:
                 self.view.bot.logger.pushError("[PAGE] Error in dropdown callback with value: `{}` for search type: `{}`:".format(self.values[0], self.view.stype), e)
+                # enable close button if it hasn't been enabled, so the user can return to pages
                 self.view.close_b.disabled=False
                 self.view.close_b.style=disnake.ButtonStyle.danger
                 self.view.close_b.label = "Close"
                 self.disabled=False
+                # set error message
                 await inter.edit_original_message(embed=self.view.bot.embed(title="Error", description="An unexpected error occured.\nMy owner has been notified.\nIf the problem persists, please wait for a fix.", color=self.view.color), view=self.view)
         else:
             await inter.response.send_message("You can't use this dropdown", ephemeral=True)
@@ -133,6 +145,7 @@ class RankingDropdown(disnake.ui.StringSelect):
 class PageRanking(BaseView):
     """__init__()
     Constructor
+    Note: contrary to its name, it's not an inheritor of the Page class
     
     Parameters
     ----------
@@ -147,19 +160,22 @@ class PageRanking(BaseView):
     """
     def __init__(self, bot : 'DiscordBot', owner_id : int, embeds : list, search_results : list, color, stype : bool, timeout : float = 180.0, enable_timeout_cleanup : bool = False) -> None:
         super().__init__(bot, owner_id=owner_id, timeout=timeout, enable_timeout_cleanup=enable_timeout_cleanup)
-        self.current = 0
-        self.embeds = embeds
-        self.search_results = search_results
-        self.color = color
-        self.stype = stype
-        self.prev_b = self.children[0]
-        self.prev_b.disabled=(len(embeds)<2)
+        self.current = 0 # current page/embed
+        self.embeds = embeds # list of pages/embeds
+        self.search_results = search_results # list of list for each pages. A list = a dropdown selection for that page
+        self.color = color # embeds color
+        self.stype = stype # type of content, True for crews, False for players
+        # buttons
+        self.prev_b = self.children[0] # Note: they are in order of the callback definitions below
+        self.prev_b.disabled=(len(embeds)<2) # disable if single page
         self.close_b = self.children[1]
         self.close_b.disabled=True
         self.next_b = self.children[2]
-        self.next_b.disabled=(len(embeds)<2)
+        self.next_b.disabled=(len(embeds)<2) # disable if single page
+        # dropdown
         self.add_item(RankingDropdown("Open Crew Page" if self.stype else "Open Profile", search_results[0]))
         self.dropdown = self.children[-1]
+        # remove previous and next buttons if single page
         if len(embeds) < 2:
             self.remove_item(self.prev_b).remove_item(self.next_b)
 
@@ -175,12 +191,15 @@ class PageRanking(BaseView):
     @disnake.ui.button(label='◀️', style=disnake.ButtonStyle.blurple)
     async def prev(self, button: disnake.ui.Button, interaction: disnake.Interaction) -> None:
         try:
-            if not button.disabled and self.ownership_check(interaction):
+            if not button.disabled and self.ownership_check(interaction): # check if enabled and author is view owner
                 if len(self.embeds) > 0:
+                    # cycle to previous page
                     self.current = (self.current + len(self.embeds) - 1) % len(self.embeds)
                     self.close_b.label = "Page {}".format(self.current + 1)
                     await interaction.send("\u200b", ephemeral=True, delete_after=0)
+                    # update dropdown
                     self.dropdown.update_options(self.search_results[self.current])
+                    # update message
                     await self.message.edit(embed=self.embeds[self.current], view=self)
                 else:
                     await interaction.send("Impossible to change pages", ephemeral=True)
@@ -201,20 +220,23 @@ class PageRanking(BaseView):
     """
     @disnake.ui.button(label='Page 1', style=disnake.ButtonStyle.secondary)
     async def close_profile(self, button: disnake.ui.Button, interaction: disnake.Interaction) -> None:
-        if not button.disabled and self.ownership_check(interaction):
+        if not button.disabled and self.ownership_check(interaction): # check if enabled and author is view owner
             if len(self.embeds) > 0:
                 await interaction.send("\u200b", ephemeral=True, delete_after=0)
-                button.disabled=False
+                # disable this button
+                button.disabled=True
                 button.style=disnake.ButtonStyle.secondary
                 button.label = "Page {}".format(self.current + 1)
+                # enable prev and next buttons if they exist
                 if len(self.embeds) >= 2:
                     self.prev_b.disabled=False
                     self.prev_b.style=disnake.ButtonStyle.blurple
                     self.next_b.disabled=False
                     self.next_b.style=disnake.ButtonStyle.blurple
+                # update message
                 await self.message.edit(embed=self.embeds[self.current], view=self)
             else:
-                await interaction.send("Impossible to change pages", ephemeral=True)
+                await interaction.send("Impossible to close this Profile", ephemeral=True)
         else:
             await interaction.response.send_message("You can't press this button", ephemeral=True)
 
@@ -230,12 +252,15 @@ class PageRanking(BaseView):
     @disnake.ui.button(label='▶️', style=disnake.ButtonStyle.blurple)
     async def next(self, button: disnake.ui.Button, interaction: disnake.Interaction) -> None:
         try:
-            if not button.disabled and self.ownership_check(interaction):
+            if not button.disabled and self.ownership_check(interaction): # check if enabled and author is view owner
                 if len(self.embeds) > 0:
+                    # cycle to previous page
                     self.current = (self.current + 1) % len(self.embeds)
                     self.close_b.label = "Page {}".format(self.current + 1)
                     await interaction.send("\u200b", ephemeral=True, delete_after=0)
+                    # update dropdown
                     self.dropdown.update_options(self.search_results[self.current])
+                    # update message
                     await self.message.edit(embed=self.embeds[self.current], view=self)
                 else:
                     await interaction.send("Impossible to change pages", ephemeral=True)
