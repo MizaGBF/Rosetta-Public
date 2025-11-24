ee6d1be01621dcac5b446e20f2ec7d0fb6d8e632
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2025-11-24 20:20:50 +0100

    12.6.2 version bump

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ import traceback
 
 # Main Bot Class (overload commands.Bot)
 class DiscordBot(commands.InteractionBot):
-    VERSION : str = "12.6.1" # bot version
+    VERSION : str = "12.6.2" # bot version
     CHANGELOG : list[str] = [ # changelog lines
         (
             "Please use `/bug_report`, open an [issue]"
